0301be8efd3be9fe83bf1e5b2a4b1836a13979af
ervinsv92 <ervinsv92@gmail.com>
2022-10-20 07:02:55 -0600

    0002 Operadores parte 1

---
+++ 0002_operadores/0002_operadores.js
@@ -0,0 +1,107 @@
+/**
+ * El código se va ejecutando de arriba hacia abajo, línea por línea
+ * Las líneas que tienen comentarios, son omitidas por el interprete del navegador
+ */
+
+//Operadores de asignacion
+//Asignacion, ej, le asigno el valor de 10 a la variable numero
+let numero = 10;
+
+//Suma
+//Forma larga
+let numeroSumaLarga = 4;
+numeroSumaLarga = numeroSumaLarga + 2;
+console.log('Suma - Forma larga (4+2): ',numeroSumaLarga)
+
+//Forma corta
+let numeroSumaCorta = 6;
+numeroSumaCorta +=6;
+console.log('Suma - Forma corta (6+6): ',numeroSumaCorta)
+
+//Resta
+//Forma larga
+let numeroRestaLarga = 4;
+numeroRestaLarga = numeroRestaLarga - 2;
+console.log('Resta - Forma larga (4-2): ',numeroRestaLarga)
+
+//Forma larga
+let numeroRestaCorta = 10;
+numeroRestaCorta -= 5;
+console.log('Resta - Forma corta(10-5): ',numeroRestaCorta)
+
+//Multiplicacion
+//Forma larga
+let numeroMultiLarga = 4;
+numeroMultiLarga = numeroMultiLarga * 2;
+console.log('Multi - Forma larga (4*2): ',numeroMultiLarga)
+
+//Forma larga
+let numeroMultiCorta = 10;
+numeroMultiCorta *= 5;
+console.log('Multi - Forma corta (10*5): ',numeroMultiCorta)
+
+//Division
+//Forma larga
+let numeroDivLarga = 4;
+numeroDivLarga = numeroDivLarga / 2;
+console.log('Division - Forma larga (4/2): ',numeroDivLarga)
+
+//Forma larga
+let numeroDivCorta = 10;
+numeroDivCorta /= 5;
+console.log('Division - Forma corta (10/5): ',numeroDivCorta)
+
+//Residuo
+//Forma larga
+let numeroResiduoLarga = 4;
+numeroResiduoLarga = numeroResiduoLarga % 2;
+console.log('Residuo - Forma larga (4%2): ',numeroResiduoLarga)
+
+//Forma larga
+let numeroResiduoCorta = 10;
+numeroResiduoCorta %= 5;
+console.log('Residuo - Forma corta (10%5): ',numeroResiduoCorta)
+
+//Exponente
+//Forma larga
+let numeroExpoLarga = 4;
+numeroExpoLarga = numeroExpoLarga ** 2;
+console.log('Exponente - Forma larga (4**2): ',numeroExpoLarga)
+
+//Forma larga
+let numeroExpoCorta = 10;
+numeroExpoCorta **= 5;
+console.log('Exponente - Forma corta (10**5): ',numeroExpoCorta)
+
+//Operadores aritmeticos
+//Aumento en 1
+let aumentoEnUno = 10;
+aumentoEnUno++;
+console.log('Aumento en uno (10)', aumentoEnUno);
+
+//Resta en 1
+let restaEnUno = 10;
+restaEnUno--;
+console.log('Resta en uno (10)', restaEnUno);
+
+//Prueba de suma
+let resultado = 0;
+let edad1 = 10;
+let edad2 = 12;
+resultado = edad1 + edad2;
+console.log('Resultado de sumar (10+12): ', resultado);
+
+//Operadores logicos (true, false)
+//Operador normal
+let operador = true;
+console.log('Operador (previo true): ', operador);//true
+
+//Operador negado (inverir la variable operador)
+console.log('Operador invertido (previo true): ', !operador);//false
+console.log('Operador: ', operador);//true
+
+//Operadores logicos
+//AND (&&)
+
+//OR (||)
+
